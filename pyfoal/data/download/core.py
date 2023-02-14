c2a78760875ae7d040f53442a77f66ee767f82f9
Max Morrison <max@descript.com>
2023-02-14 23:05:43 +0000

    Evaluation accuracy

--- pyfoal/data/download/core.py
+++ pyfoal/data/download/core.py
@@ -86,11 +86,11 @@ def arctic():
     """Download arctic dataset"""
     # Delete data if it already exists
     data_directory = pyfoal.DATA_DIR / 'arctic'
-    # if data_directory.exists():
-    #     shutil.rmtree(str(data_directory))
+    if data_directory.exists():
+        shutil.rmtree(str(data_directory))
 
     # Create data directory
-    # data_directory.mkdir(parents=True)
+    data_directory.mkdir(parents=True)
 
     # URL format string
     url = (
@@ -102,12 +102,12 @@ def arctic():
         ARCTIC_SPEAKERS,
         'Downloading arctic',
         total=len(ARCTIC_SPEAKERS))
-    # for speaker in iterator:
-    #     download_tar_bz2(url.format(speaker), data_directory)
+    for speaker in iterator:
+        download_tar_bz2(url.format(speaker), data_directory)
 
     # Download text data
     text_file = data_directory / 'sentences.txt'
-    # download_file('http://festvox.org/cmu_arctic/cmuarctic.data', text_file)
+    download_file('http://festvox.org/cmu_arctic/cmuarctic.data', text_file)
 
     # Setup data directory
     cache_directory = pyfoal.CACHE_DIR / 'arctic'
--- pyfoal/evaluate/metrics.py
+++ pyfoal/evaluate/metrics.py
@@ -11,13 +11,15 @@ import pyfoal
 class Metrics:
 
     def __init__(self):
+        self.accuracy = Accuracy()
         self.l1 = L1()
         self.loss = Loss()
 
     def __call__(self):
+        results = {**self.accuracy(), **self.loss()}
         if self.l1.count > 0:
-            return {**self.l1(), **self.loss()}
-        return self.loss()
+            return {**results, **self.l1()}
+        return results
 
     def update(
         self,
@@ -32,8 +34,9 @@ class Metrics:
         # Update loss
         self.loss.update(logits.detach(), phoneme_lengths, frame_lengths)
 
-        # Update phoneme duration rmse
+        # Update phoneme duration accuracy and error
         if alignments is not None and targets is not None:
+            self.accuracy.update(alignments, targets)
             self.l1.update(alignments, targets)
 
     def reset(self):
@@ -44,6 +47,37 @@ class Metrics:
 ###############################################################################
 # Individual metrics
 ###############################################################################
+
+
+class Accuracy:
+
+    def __init__(self, levels=[.01, .005, .0025, .00125]):
+        self.levels = levels
+        self.reset()
+    
+    def __call__(self):
+        return {
+            f'accuracy-{level}': (self.totals[level] / self.count).item()
+            for level in self.levels}
+
+    def update(self, alignments, targets):
+        for alignment, target in zip(alignments, targets):
+            
+            # Extract phoneme durations
+            predicted_durations = torch.tensor([
+                phoneme.duration() for phoneme in alignment.phonemes()])
+            target_durations = torch.tensor([
+                phoneme.duration() for phoneme in target.phonemes()])
+        
+            # Update
+            for level in self.levels:
+                self.total[level] += sum(
+                    torch.abs(predicted_durations - target_durations) < level)
+                self.count += predicted_durations.numel()
+
+    def reset(self):
+        self.count = 0
+        self.total = {level: 0. for level in self.levels}
 
 
 class L1:
